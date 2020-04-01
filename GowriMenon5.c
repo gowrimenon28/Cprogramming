913f881f8bf71f146c81dd17fc6fd54b7a2fc5d8
gowrimenon28 <62933970+gowrimenon28@users.noreply.github.com>
2020-04-02 00:02:17 +0530

    Update GowriMenon5.c

--- GowriMenon5.c
+++ GowriMenon5.c
@@ -3,11 +3,9 @@
 void main()
 
 {
-  char petname []="chikku";
-  char petfood []="meat,biscuit";
-  printf("My petname is %s \n",petname);
-  printf("Age of my dog is %d \n",6);
-  printf("Favourite petfood for my dog are %s \n",petfood);
+  printf("My petname is %s \n","chikku");
+  printf("Age of my dog is %d. \n",6);
+  printf("Favourite petfood for my dog is %s. \n","biscuit");
   getch();
 
-}
+} 
